9e9c2441ed276e624d50856e04c923dd145f1eea
Carlos metaute hoyos <carlosmet9409@gmail.com>
2023-08-28 14:19:26 -0500

    bg test2

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main style={{backgroundImage: `url('https://hips.hearstapps.com/hmg-prod/images/911-dakar-19-1668642643.jpg')`, backgroundSize:'cover'}}>
+    <main className="w-screen h-screen" style={{backgroundImage: `url('https://hips.hearstapps.com/hmg-prod/images/911-dakar-19-1668642643.jpg')`, backgroundSize:'cover'}}>
       hola
     </main>
   )
